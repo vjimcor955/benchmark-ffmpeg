43f76979f7b9107f9e8c487ba865249f752d76e6
vjimene1 <vjimene1@uni-koeln.de>
2024-03-14 13:30:02 +0100

    add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h3>FFmpeg Benchmark</h3>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <!-- <RouterLink to="/about">Results</RouterLink> -->
+    </nav>
+  </header>
+</template>
+
+
+<script>
+  import { RouterLink } from 'vue-router'
+
+  export default {
+    
+  }
+</script>
+
+
+<style lang="scss">
+  header {
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 10rem;
+    background-color: #f4f4f4;
+
+    nav {
+      display: flex;
+      gap: 5rem;
+    }
+  }
+
+</style>
