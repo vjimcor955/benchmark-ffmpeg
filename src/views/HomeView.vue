f78d14d1096af462fbdc8246510510c2e4e45b94
vjimene1 <vjimene1@uni-koeln.de>
2024-03-14 13:30:15 +0100

    add Home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>HOME VIEW</h1>
+  </div>
+</template>
+
+
+<script>
+  
+</script>
+
+
+<style lang="scss">
+  div.home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 80px);
+  }
+  
+</style>
