a40ff6f07728eff58d901cc5279e3a8dd02b0fd9
vjimene1 <vjimene1@uni-koeln.de>
2024-03-14 13:30:25 +0100

    add Results view

---
+++ src/views/ResultsView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="results">
+    <h1>RESULTS VIEW</h1>
+    
+  </div>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style lang="scss">
+  div.results {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 80px);
+  }
+
+</style>
