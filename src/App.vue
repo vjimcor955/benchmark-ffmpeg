14445a03e204a2b40c6627eb8a0402591db2dd19
vjimene1 <vjimene1@uni-koeln.de>
2024-03-14 13:29:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <Header />
+    <RouterView />
+  </main>
+</template>
+
+
+<script>
+  import { RouterView } from 'vue-router';
+  import Header from './components/Header.vue';
+  
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/results',
+      name: 'results',
+      component: () => import('../views/ResultsView.vue')
+    }
+  ]
+})
+
+export default router
