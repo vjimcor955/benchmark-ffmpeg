0d31ea2d4a77d157f15a8a84713e8e5e1fa9710e
vjimcor955 <vjimcor955@g.educaand.es>
2024-03-19 15:58:25 +0100

    Add more commands if wanted

--- src/components/CommandForm.vue
+++ src/components/CommandForm.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="command">
-    <h1>FFmpeg command</h1>
+    <h1>FFmpeg commands</h1>
     <form class="command__form">
       <div class="command__form--field">
-        <label for="line">Write the ffmpeg command:</label>
+        <label for="line">Write the ffmpeg commands:</label>
         <div class="command__form--field--input">
           <p>ffmpeg -i {{ inputVideo }}</p>
-          <input v-model="line" type="text" id="line">
+          <input v-model="command1" type="text" id="line">
         </div>
+
+        <div v-if="showCommand2" class="command__form--field--input">
+          <p>ffmpeg -i {{ inputVideo }}</p>
+          <input v-model="command2" type="text" id="line">
+          <a @click="showCommand2 = !showCommand2">X</a>
+        </div>
+        <a v-if="!showCommand2" @click="showCommand2 = !showCommand2" class="add_command">Add Command</a>
+
+        <div v-if="showCommand3" class="command__form--field--input">
+          <p>ffmpeg -i {{ inputVideo }}</p>
+          <input v-model="command3" type="text" id="line">
+          <a @click="showCommand3 = !showCommand3">X</a>
+        </div>
+        <a v-if="showCommand2 && !showCommand3" @click="showCommand3 = !showCommand3" class="add_command">Add Command</a>
+
       </div>
       <input type="submit" value="Run command test" @click="runCommandTest">
     </form>
@@ -23,7 +38,11 @@
     data() {
       return {
         inputVideo: this.video.name,
-        line: '-c:v libx265 results/prueba_comando.mp4',
+        command1: '',
+        command2: '',
+        command3: '',
+        showCommand2: false,
+        showCommand3: false,
       }
     },
     props: {
@@ -33,12 +52,25 @@
       }
     },
     methods: {
+      /**
+       * Runs the tests for the written commands.
+       * @param {Event} e - The event object.
+       * @returns {Promise<void>}
+       */
       async runCommandTest(e) {
         e.preventDefault()
-        this.uploadVideo(this.line)
+        const allCommand = [this.command1, this.command2, this.command3]
+        allCommand.forEach(command => {
+          this.commandTest(command)
+        })
       },
 
-      async uploadVideo(command) {
+      /**
+       * Executes the test by sending a video file and a command to the server.
+       * @param {string} command - The command to be executed on the server.
+       * @returns {Promise<void>} - A promise that resolves when the command test is completed.
+       */
+      async commandTest(command) {
         const formData = new FormData()
         formData.append('video', this.video)
         try {
@@ -76,7 +108,6 @@
     width: 500px;
     flex-direction: column;
     justify-content: center;
-    // align-items: flex-start;
     align-items: center;
     width: 80vw;
     gap: 40px;
@@ -85,8 +116,8 @@
       width: 100%;
       display: flex;
       flex-direction: column;
-      gap: 5px;
-      text-align: center;
+      gap: 25px;
+      align-items: center;
 
       label {
         font-size: 1.1em;
@@ -94,18 +125,30 @@
       }
 
       &--input {
+        width: 75%;
         display: flex;
         gap: 15px;
         align-items: center;
         justify-content: center;
 
         input[type="text"] {
-          width: 75%;
+          width: 85%;
           font-size: 1.1em;
           padding: 10px;
           border: 1px solid #000;
           border-radius: 5px;
         }
+      }
+
+      .add_command {
+        width: fit-content;
+        cursor: pointer;
+        text-decoration: underline;
+        padding: 10px;
+        border-radius: 5px;
+      }
+      .add_command:hover {
+        background-color: #f0f0f0;
       }
     }
 
