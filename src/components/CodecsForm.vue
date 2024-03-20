c7558937702405ffc28511f6bb9653cfd711066c
vjimcor955 <vjimcor955@g.educaand.es>
2024-03-20 10:50:41 +0100

    CodecsForm refactoring and design

--- src/components/CodecsForm.vue
+++ src/components/CodecsForm.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="codecs">
-    <h1>Codecs Selector</h1>
-    <form class="codecs__form">
-      <div class="codecs__form--field">
-        <label for="codec1">Write a video codec:</label>
-        <input v-model="codec1" type="text" id="codec1">
+    <form class="codec__form">
+      <h1>Codecs Selector</h1>
+      <p>Write the video codecs:</p>
+      <div class="codec__form--fields">
+        <div class="codec__form--fields--input">
+          <label for="codec1">Codec 1:</label>
+          <input v-model="codec1" type="text" id="codec1">
+        </div>
+        <div v-if="showCodec2" class="codec__form--fields--input">
+          <label for="codec2">Codec 2:</label>
+          <input v-model="codec2" type="text" id="codec2">
+          <a @click="showCodec2 = !showCodec2" class="delete_field">X</a>
+        </div>
+        <div v-if="showCodec3" class="codec__form--fields--input">
+          <label for="codec3">Codec 3:</label>
+          <input v-model="codec3" type="text" id="codec3">
+          <a @click="showCodec3 = !showCodec3" class="delete_field">X</a>
+        </div>
       </div>
-      <div class="codecs__form--field">
-        <label for="codec2">Write a video a codec:</label>
-        <input v-model="codec2" type="text" id="codec2">
-      </div>
-      <div class="codecs__form--field">
-        <label for="codec3">Write a video a codec:</label>
-        <input v-model="codec3" type="text" id="codec3">
-      </div>
+      <a v-if="!showCodec2" @click="showCodec2 = !showCodec2" class="add_codec">Add Codec</a>
+      <a v-if="showCodec2 && !showCodec3" @click="showCodec3 = !showCodec3" class="add_codec">Add Codec</a>
       <input type="submit" value="Run codecs tests" @click="runCodecsTests">
     </form>
   </div>
@@ -30,6 +37,8 @@
         codec1: '',
         codec2: '',
         codec3: '',
+        showCodec2: false,
+        showCodec3: false,
       }
     },
     props: {
@@ -51,7 +60,6 @@
           this.codecTest(codec)
         })
       },
-
       /**
        * Sends a POST request to test a codec with the provided video file.
        * @param {string} codec - The codec to test.
@@ -78,40 +86,68 @@
 
 
 <style lang="scss">
-  h1 {
-    margin: 40px;
-    font-size: 1.2rem;
-    text-align: center;
-    font-weight: bold;
-  }
-
-  .codecs__form {
+  .codec__form {
     display: flex;
-    width: 500px;
+    width: 50vw;
+    padding: 50px;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 80vw;
     gap: 40px;
+  
+    h1 {
+      font-size: 1.5rem;
+      text-align: center;
+      font-weight: bold;
+    }
 
-    &--field {
-      width: 350px;
+    &--fields {
+      width: 550px;
       display: flex;
       flex-direction: column;
-      gap: 5px;
+      gap: 20px;
+      align-items: flex-start;
 
-      label {
-        font-size: 1.1em;
-        margin-bottom: 10px;
+      &--input {
+        width: 100%;
+        display: flex;
+        gap: 15px;
+        align-items: center;
+      
+        label {
+          font-size: 1.1em;
+        }
+  
+        input[type="text"] {
+          width: 70%;
+          font-size: 1.1em;
+          padding: 10px;
+          border: 1px solid #000;
+          border-radius: 5px;
+        }
+
+        .delete_field {
+          cursor: pointer;
+          padding: 10px;
+          border-radius: 50%;
+        }
+        .delete_field:hover {
+          background-color: #f0f0f0;
+        }
       }
+    }
+      
+    .add_codec {
+      width: fit-content;
+      cursor: pointer;
+      text-decoration: underline;
+      padding: 5px;
+      border-radius: 5px;
+      // border: #000 1px solid;
 
-      input[type="text"] {
-        font-size: 1.1em;
-        padding: 10px;
-        border: 1px solid #000;
-        border-radius: 5px;
-      }
-
+    }
+    .add_codec:hover {
+      background-color: #f0f0f0;
     }
 
     input[type="submit"] {
