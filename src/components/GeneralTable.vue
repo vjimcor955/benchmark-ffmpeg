85f6cb168bdad03120c73b8a157e55ba6ae081bb
vjimcor955 <vjimcor955@g.educaand.es>
2024-03-27 14:52:01 +0100

    create GeneralTable component

---
+++ src/components/GeneralTable.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="general__table">
+    <table>
+      <thead>
+        <tr>
+          <th>Option</th>
+          <th>Average</th>
+          <th>Max</th>
+          <th>Median</th>
+          <th>Min</th>
+          <th>Stdev</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="metric in tableRows">
+          <td>{{ metric[0] }}</td>
+          <td>{{ metric[1] }}</td>
+          <td>{{ metric[2] }}</td>
+          <td>{{ metric[3] }}</td>
+          <td>{{ metric[4] }}</td>
+          <td>{{ metric[5] }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'GeneralTable',
+    data() {
+      return {
+        tableRows: [],
+      }
+    },
+    props: {
+      codecData: {
+        type: Object,
+        required: true
+      }
+    },
+    created() {
+      this.parseDataTable(this.codecData)
+    },
+    methods: {
+      /**
+       * Parses the data and pushes it into the table.
+       * @param {Object} codecData - Data to be parsed.
+       */
+       parseDataTable(data) {
+        this.tableRows = []
+
+        const psnr = data.quality_metrics.global.psnr.mse_avg
+        const ssim = data.quality_metrics.global.ssim.ssim_avg
+        const vmaf = data.quality_metrics.global.vmaf.vmaf
+
+        this.tableRows.push(["PSNR", psnr.average, psnr.max, psnr.median, psnr.min, psnr.stdev])
+        this.tableRows.push(["SSIM", ssim.average, ssim.max, ssim.median, ssim.min, ssim.stdev])
+        this.tableRows.push(["VMAF", vmaf.average, vmaf.max, vmaf.median, vmaf.min, vmaf.stdev])
+      },
+    }
+  }
+</script>
+
+
+<style lang="scss">
+  .general__table {
+    width: 100%;
+    display: flex;
+    text-align: center;
+
+    table {
+      width: 100%;
+      border-collapse: collapse;
+
+      th {
+        align-items: center;
+        border: 1px solid #000;
+        padding: 10px;
+        font-weight: bold;
+      }
+
+      td {
+        align-items: center;
+        border: 1px solid #000;
+        padding: 10px;
+      }
+    }
+  }
+</style>
