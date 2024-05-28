da4436d95839fb409155a1dc10f391eed674a203
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-28 19:54:26 +0200

    header style implemented

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,16 @@
 <template>
-  <header>
-    <h3>FFmpeg Benchmark</h3>
-    <nav>
-      <RouterLink :to="{name: 'home'}">Home</RouterLink>
-      <!-- <RouterLink :to="{name: 'results'}">Results</RouterLink> -->
-      <RouterLink :to="{name: 'player'}">Player</RouterLink>
+  <header class="header">
+    <h3 class="header__title">FFmpeg Benchmark</h3>
+    <nav class="header__navbar">
+      <!-- public layout -->
+      <RouterLink :to="{name: 'home'}" class="header__navbar link">Home</RouterLink>
+      <!-- <RouterLink :to="{name: 'sign-in'}" class="header__navbar button">Sign in</RouterLink> -->
+      <!-- private layout -->
+      <!-- <RouterLink :to="{name: 'user'}" class="header__navbar link">User</RouterLink> -->
+      <!-- <RouterLink :to="{name: 'convert-video'}" class="header__navbar link__button">Convert video</RouterLink> -->
+      <!-- <button class="header__navbar link__button">Log out</button> -->
+
+      <RouterLink :to="{name: 'player'}" class="header__navbar link__button">Sign in</RouterLink>
     </nav>
   </header>
 </template>
@@ -20,18 +26,30 @@
 
 
 <style lang="scss">
-  header {
-    height: 80px;
+  @import "../assets/sass/main.scss";
+
+  .header {
+    height: 70px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0px 10rem;
-    background-color: #f4f4f4;
+    background-color: rgba($color: $secondary_color, $alpha: 1);
+    color: #fff;
+    @include box-gradient($secondary_color);
 
-    nav {
+    &__title {
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+
+    &__navbar {
       display: flex;
       gap: 5rem;
+      align-items: center;
     }
-  }
 
+    @include link($color: #fff);
+    @include link__button($color: #fff);
+  }
 </style>
