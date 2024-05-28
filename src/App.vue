79cf193d6efdb8871c5f5793d11a878ee9327b10
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-28 20:00:31 +0200

    import footer and main style implemented

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <main>
     <Header />
     <RouterView />
+    <Footer />
   </main>
 </template>
 
@@ -9,15 +10,23 @@
 <script>
   import { RouterView } from 'vue-router';
   import Header from './components/Header.vue';
-  
+  import Footer from './components/Footer.vue';
+
   export default {
     components: {
-      Header
+      Header,
+      Footer
     }
   }
 </script>
 
 
-<style scoped>
+<style scoped lang="scss">
+  @import "./assets/sass/main.scss";
 
+  main {
+    min-height: calc(100vh - 140px);
+    font-family: "Nunito";
+    background-color: $background-color;
+  }
 </style>
