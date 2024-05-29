91ec036cc63c25e7ff30fe216cac3066dfeeeae4
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-29 11:48:15 +0200

    root and private layouts created

---
+++ src/layouts/LayoutPrivate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout_private">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import {useAuthStore} from '../stores/authStore.js'
+
+  export default {
+    name: 'LayoutPrivate',
+    computed: {
+      isLogged() {
+        return useAuthStore().isLogged
+      }
+    },
+    // If user is not logged cant access to layoutPrivate, redirect to login page
+    created() {
+      if (!this.isLogged) {
+        this.$router.push('/login')
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .layout_private {
+    min-height: calc(100vh - 120px);
+    font-family: "Nunito";
+  }
+</style>
