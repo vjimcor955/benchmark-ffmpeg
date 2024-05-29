46c98b95c4235abd614096420fc9a1a4c28fed7b
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-29 11:45:19 +0200

    create authStore

---
+++ src/stores/authStore.js
@@ -0,0 +1,53 @@
+import {defineStore} from 'pinia'
+
+export const useAuthStore = defineStore('authStore', {
+  state: () => ({
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      user: '',
+      password: '',
+      token: ''
+    },
+    isLogged: false,
+  }),
+  actions: {
+    logIn(user) {
+      this.user = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        user: user.user,
+        password: user.password,
+        token: user.token
+      }
+      this.isLogged = true
+    },
+    logOut() {
+      this.user = {
+        id: '',
+        name: '',
+        email: '',
+        user: '',
+        password: '',
+        token: ''
+      }
+      this.isLogged = false
+    },
+    updateUserRanking(ranking) {
+      this.user.ranking = ranking
+    }
+  },
+  persist: {
+    enabled: true,
+    strategies : [
+      {
+        key: "auth",
+        storage: localStorage,
+      }
+    ]
+  }
+})
+
+export default useAuthStore
