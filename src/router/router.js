8baa0274882f4b29ddd1f0d0832e95701a30f682
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-29 11:47:31 +0200

    router refactoring

--- src/router/router.js
+++ src/router/router.js
@@ -58,26 +58,3 @@ const router = createRouter({
 })
 
 export default router
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView
-//     },
-//     {
-//       path: '/results',
-//       name: 'results',
-//       component: () => import('../views/ResultsView.vue')
-//     },
-//     {
-//       path: '/player',
-//       name: 'player',
-//       component: () => import('../views/VideoPlayerView.vue')
-//     }
-//   ]
-// })
-
-// export default router
