f8201690e68fb3c0e93d3539b3eeb61d631026b8
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-29 11:52:40 +0200

    import pinia-plugin-persist and refactoring

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import './assets/sass/utilities/reset.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersist from 'pinia-plugin-persist'
 
 import App from './App.vue'
-import router from './router'
+import router from './router/router.js'
 
 import 'vue3-toastify/dist/index.css';
 
-const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersist)
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .mount('#app')
