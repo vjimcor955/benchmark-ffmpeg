3cc56f9f919963687ff4b718dc1bae0077e8e807
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-31 21:20:04 +0200

    LoginView completed

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,34 +1,62 @@
 <template>
-  <div class="login">
-    <h1>LOGIN</h1>
-    <button @click="handleLogin">Login</button>
-</div>
+  <div class="loginView">
+    <div class="form">
+      <LoginForm v-if="!register"/>
+      <RegisterForm v-else @registered="handleRegistered"/>
+      <a @click="register = !register" class="link">{{ !register ? 'Register' : 'Login' }}</a>
+    </div>
+  </div> 
 </template>
 
 
 <script>
-  import {useAuthStore} from '../stores/authStore.js'
+  import LoginForm from '../components/LoginForm.vue'
+  import RegisterForm from '../components/RegisterForm.vue'
 
   export default {
-    name: 'LoginView',
+    data() {
+      return {
+        register: false
+      }
+    },
+    components: {
+      LoginForm,
+      RegisterForm
+    },
     methods: {
-      handleLogin() {
-        setTimeout(() => {
-          useAuthStore().isLogged = true;
-          this.$router.push({name: "root-home"});
-        }, 1000);      }
+      handleRegistered(value) {
+        this.register = value;
+      }
     }
   }
+
 </script>
 
+<style scoped lang="scss">
+  @import "../assets/sass/main.scss";
 
-<style lang="scss">
-  .login {
+  .loginView {
     min-height: inherit;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     gap: 100px;
+    
+    .form {
+      min-height: 500px;
+      width: 400px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 25px;
+      padding: 25px 50px;
+      border-radius: 15px;
+      background-color: $components_color;
+      @include box-shadow($details_color);
+
+      @include link($accent_color);
+    }
   }
 </style>
